7c930504c39830d47b6f97f4e4c7c5129dd52828
AAyinkx <ayinkafinowi@gmail.com>
2024-09-02 09:29:32 +0100

    Added some comments to javascript file

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-mybutton = document.getElementById("myBtn");
-//I have created a varieable that will be used for the scroll to top button (fetched by id in html)
+const mybutton = document.getElementById("myBtn");
+//I have created a constant varieable that will be used for the scroll to top button (fetched by id in html)
 
 // When the user scrolls the function named scrollFunction is enacted
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 //If the user scrolls more than 20px from the top of the page the button will appear otherwise it won't be visible
 function scrollFunction() {
-  if (document.documentElement.scrollTop > 20)  { 
+  if (document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-mybutton.addEventListener('click', topFunction);
-
+mybutton.addEventListener("click", topFunction);
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.documentElement.scrollTop = 0; //If the 'scroll to top' button is clicked the scrollTop value is set to 0 so the user will taken to the top
+  document.documentElement.scrollTop = 0; //If the 'scroll to top' button is clicked the scrollTop value is set to 0 so the user will taken to the top
 }
-
